7579849ad4cd9e7b2405b124f2c07f63ba5ba33c
Ayush Agarwal <43469729+python-freak@users.noreply.github.com>
2019-01-27 23:13:15 +0530

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import scipy
 from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
-from NN import *
+from helper_functions import *
 
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
